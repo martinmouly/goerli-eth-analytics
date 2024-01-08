78001be105fbd6d936c9dae03ee0a691a74e4377
Martin Mouly <martin@mouly.name>
2024-01-08 15:59:21 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,1 +1,20 @@
+// get balance of given account
+web3.fromWei(eth.getBalance('0xdDCc9d80602F8C37eD82C040BD22d19DB28ED987'), 'ether')
+
+// get list of all connected nodes (IP addresses)
+admin.peers.forEach(function(value){console.log(value.network.remoteAddress+"\t"+value.name)})
+
+// add a peer
+admin.addPeer(<valeur de l'enode remote obtenue à l'aide de admin.nodeInfo>)
+
+// retrieve list of transactions in given block
+eth.getBlock(8017276).transactions.forEach(function(value){console.log(value)})
+
+// list of transactions in mempool
+txpool.content // full details of each transactions
+txpool.inspect // quick summary of each transaction
+
+// list of all events in given transaction
+eth.getTransactionReceipt("0x78482d3e5b5c3f4fd73df433343ab83160a90af8a0aaa25259c3117965af5d3d")
+
 
